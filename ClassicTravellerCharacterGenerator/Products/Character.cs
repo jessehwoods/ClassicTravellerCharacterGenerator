1e3974ec3d9f6c2b96eeb9d08af559609f5bffaf
jessehwoods <jessehwoods@gmail.com>
2022-07-04 10:21:12 -0400

    Moved property and method comments from Character to ICharacter.

--- ClassicTravellerCharacterGenerator/Products/Character.cs
+++ ClassicTravellerCharacterGenerator/Products/Character.cs
@@ -11,65 +11,35 @@ namespace TravellerCharacterGenerator.Products
     ///  This is used to hold the character information at the end of character creation. It is currently intended only to be used as an accessor.
     internal class Character : ICharacter
     {
-        /// <summary>
-        /// Name of the character.
-        /// </summary>
+
         public string Name { get { return name; } }
-        /// <summary>
-        /// Age of the character in years.
-        /// </summary>
+
         public int AgeYears { get { return career.AgeYears; } }
-        /// <summary>
-        /// Age of the character in months, with years removed.
-        /// </summary>
+
         public int AgeMonths { get { return career.AgeMonths; } }
-        /// <summary>
-        /// Terms the character spent in their career.
-        /// </summary>
+
         public int Terms { get { return career.Terms; } }
-        /// <summary>
-        /// Name of the character's career.
-        /// </summary>
+
         public string CareerName { get { return career.CareerName; } }
-        /// <summary>
-        /// Returns if the character has been commissioned.
-        /// </summary>
+
         public bool Commissioned { get { return career.Commissioned; } }
-        /// <summary>
-        /// Strength attribue, rated 1-15.
-        /// </summary>
+
         public int Strength { get { return characteristics.Strength; } }
-        /// <summary>
-        /// Dexterity attribue, rated 1-15.
-        /// </summary>
+
         public int Dexterity { get { return characteristics.Dexterity; } }
-        /// <summary>
-        /// Endurance attribue, rated 1-15.
-        /// </summary>
+
         public int Endurance { get { return characteristics.Endurance; } }
-        /// <summary>
-        /// Intelligence attribue, rated 1-15.
-        /// </summary>
+
         public int Intelligence { get { return characteristics.Intelligence; } }
-        /// <summary>
-        /// Education attribue, rated 1-15.
-        /// </summary>
+
         public int Education { get { return characteristics.Education; } }
-        /// <summary>
-        /// Social Standing attribue, rated 1-15.
-        /// </summary>
+
         public int SocialStanding { get { return characteristics.SocialStanding; } }
-        /// <summary>
-        /// Cash possessed by character.
-        /// </summary>
+
         public int Cash { get { return inventory.Cash; } }
-        /// <summary>
-        /// Character's inventory, other than cash, as an array.
-        /// </summary>
+
         public string[] Inventory { get { return inventory.ItemArray; } }
-        /// <summary>
-        /// Character's skills as an array of strings, formatted as "{skill name}-{skill level}"
-        /// </summary>
+
         public string[] Skills { get { return skills.SkillsArray; } }
 
         private string name;
@@ -120,29 +90,11 @@ namespace TravellerCharacterGenerator.Products
             this.inventory = inventory;
         }
 
-        /// <summary>
-        /// Returns rank as a string, if the character has been commissioned. 
-        /// </summary>
-        /// <remarks>
-        /// The character only has a rank name, 
-        /// </remarks>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown if trying to get a rank and the character has not been commissioned.
-        /// </exception>
         public string GetRankName()
         {
             return career.GetRankName();
         }
 
-        /// <summary>
-        /// Returns rank level as an integer from 1 to 6, if the character has been commissioned. 
-        /// </summary>
-        /// <remarks>
-        /// There is no 0 rank. A character who has not been commissioned, tracked by the Commissioned property, just doesn't have a rank on my reading of the rules.
-        /// </remarks>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown if trying to get a rank and the character has not been commissioned.
-        /// </exception>
         public int GetRankLevel()
         {
             return career.GetRankLevel();
--- ClassicTravellerCharacterGenerator/Products/ICharacter.cs
+++ ClassicTravellerCharacterGenerator/Products/ICharacter.cs
@@ -1,24 +1,90 @@
 ﻿namespace TravellerCharacterGenerator.Products
 {
+    /// <summary>
+    /// This is an interface for a character object that holds and returns information about the character.
+    /// </summary>
     internal interface ICharacter
     {
-        int AgeMonths { get; }
-        int AgeYears { get; }
-        string CareerName { get; }
-        int Cash { get; }
-        bool Commissioned { get; }
-        int Dexterity { get; }
-        int Education { get; }
-        int Endurance { get; }
-        int Intelligence { get; }
-        string[] Inventory { get; }
-        string Name { get; }
-        string[] Skills { get; }
-        int SocialStanding { get; }
-        int Strength { get; }
-        int Terms { get; }
 
+        /// <summary>
+        /// Name of the character.
+        /// </summary>
+        string Name { get; }
+        /// <summary>
+        /// Age of the character in years.
+        /// </summary>
+        int AgeYears { get; }
+        /// <summary>
+        /// Age of the character in months, with years removed.
+        /// </summary>
+        int AgeMonths { get; }
+        /// <summary>
+        /// Terms the character spent in their career.
+        /// </summary>
+        int Terms { get; }
+        /// <summary>
+        /// Name of the character's career.
+        /// </summary>
+        string CareerName { get; }
+        /// <summary>
+        /// Returns if the character has been commissioned.
+        /// </summary>
+        bool Commissioned { get; }
+        /// <summary>
+        /// Strength attribue, rated 1-15.
+        /// </summary>
+        int Strength { get; }
+        /// <summary>
+        /// Dexterity attribue, rated 1-15.
+        /// </summary>
+        int Dexterity { get; }
+        /// <summary>
+        /// Endurance attribue, rated 1-15.
+        /// </summary>
+        int Endurance { get; }
+        /// <summary>
+        /// Intelligence attribue, rated 1-15.
+        /// </summary>
+        int Intelligence { get; }
+        /// <summary>
+        /// Education attribue, rated 1-15.
+        /// </summary>
+        int Education { get; }
+        /// <summary>
+        /// Social Standing attribue, rated 1-15.
+        /// </summary>
+        int SocialStanding { get; }
+        /// <summary>
+        /// Cash possessed by character.
+        /// </summary>
+        int Cash { get; }
+        /// <summary>
+        /// Character's inventory, other than cash, as an array.
+        /// </summary>
+        string[] Inventory { get; }
+        /// <summary>
+        /// Character's skills as an array of strings, formatted as "{skill name}-{skill level}"
+        /// </summary>
+        string[] Skills { get; }
+        /// <summary>
+        /// Returns rank as a string, if the character has been commissioned. 
+        /// </summary>
+        /// <remarks>
+        /// The character only has a rank name, 
+        /// </remarks>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if trying to get a rank and the character has not been commissioned.
+        /// </exception>
         string GetRankName();
+        /// <summary>
+        /// Returns rank level as an integer from 1 to 6, if the character has been commissioned. 
+        /// </summary>
+        /// <remarks>
+        /// There is no 0 rank. A character who has not been commissioned, tracked by the Commissioned property, just doesn't have a rank on my reading of the rules.
+        /// </remarks>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if trying to get a rank and the character has not been commissioned.
+        /// </exception>
         int GetRankLevel();
     }
 }
